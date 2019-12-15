eedeeeed3575473e43d6c1d469e703d65055336e
heiki50y <45164787+heiki50y@users.noreply.github.com>
2019-12-15 15:08:04 +0200

    Byt-partner

---
+++ main.js
@@ -0,0 +1,40 @@
+function openSlideMenu() {
+  document.getElementById('side-menu').style.width = '250px';
+}
+
+function closeSlideMenu() {
+  document.getElementById('side-menu').style.width = '0';
+}
+
+function onTabClick(event) {
+    let activeTabs = document.querySelectorAll('.active');
+    
+    // deactivate existing active tab and panel
+    // for( let i = 0; i < activeTabs.length; i++) {
+    //   activeTabs[i].className = activeTabs[i].className.replace('active', '');
+    // }
+    
+    activeTabs.forEach(function(tab) {
+      tab.className = tab.className.replace('active', '');
+    });
+    
+    // activate new tab and panel
+    event.target.parentElement.className += ' active';
+    document.getElementById(event.target.href.split('#')[1]).className += ' active';
+  }
+  
+  const element = document.getElementById('nav-tab');
+  
+  element.addEventListener('click', onTabClick, false);
+
+  
+  
+  
+
+
+
+
+
+  
+  
+  

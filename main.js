3e30fe0ef9faabc15cc1030c19a98a8a121f4c87
heiki50y <45164787+heiki50y@users.noreply.github.com>
2020-01-12 17:02:03 +0200

    property

--- main.js
+++ main.js
@@ -27,6 +27,24 @@ function onTabClick(event) {
   
   element.addEventListener('click', onTabClick, false);
 
+
+  const checkList = document.getElementById('list1');
+  const items = document.getElementById('items');
+  checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
+    if (items.classList.contains('visible')) {
+      items.classList.remove('visible');
+      items.style.display = "none";
+    } else {
+      items.classList.add('visible');
+      items.style.display = "block";
+    }
+  
+  }
+  
+  items.onblur = function(evt) {
+    items.classList.remove('visible');
+  }
+
   
   
   
